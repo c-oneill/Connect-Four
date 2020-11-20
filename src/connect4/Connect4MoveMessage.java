e5d8961ac5fc6a1d48b24b4ec5a73b9a7bc09fe0
Kris Rangel <krangel@email.arizona.edu>
2020-11-19 19:23:30 -0700

    added documentation

--- src/connect4/Connect4MoveMessage.java
+++ src/connect4/Connect4MoveMessage.java
@@ -18,13 +18,44 @@ public class Connect4MoveMessage implements Serializable{
     private int col;
     private int color;
     
+    /**
+     * Constructor.
+     * 
+     * @param row - row in message
+     * @param col - column in message
+     * @param color - color of token in message (yellow = 1, red = 2)
+     */
     public Connect4MoveMessage(int row, int col, int color) {
         this.row = row;
         this.col = col;
         this.color = color; 
     }
     
+    /**
+     * <ul><b><i>getRow</i></b></ul>
+     * <ul><ul><p><code>public int getRow () </code></p></ul>
+     *
+     * Returns the row stored in the message.
+     *
+     * @return the row of the token
+     */
     public int getRow() { return row; }
+    /**
+     * <ul><b><i>getColumn</i></b></ul>
+     * <ul><ul><p><code>public int getColumn () </code></p></ul>
+     *
+     * Returns the col stored in the message.
+     *
+     * @return the col of the token
+     */
     public int getColumn() { return col; }
+    /**
+     * <ul><b><i>getColor</i></b></ul>
+     * <ul><ul><p><code>public int getColor () </code></p></ul>
+     *
+     * Returns the color stored in the message.
+     *
+     * @return the integer representing the color of the token (1 = yellow, 2 = red)
+     */
     public int getColor() { return color; }
 }
