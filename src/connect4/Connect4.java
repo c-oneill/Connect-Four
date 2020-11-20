64dbc9d99bc983793256a79a77ec8afa8a133c66
Kris Rangel <krangel@email.arizona.edu>
2020-11-19 18:01:39 -0700

    Updated documentation

--- src/connect4/Connect4.java
+++ src/connect4/Connect4.java
@@ -10,6 +10,14 @@ import javafx.application.Application;
  */
 public class Connect4 {
     
+    /**
+     * <ul><b><i>main</i></b></ul>
+     * <ul><ul><p><code>public static void main (String[] args) </code></p></ul>
+     *
+     * Launches the Connect4 game.
+     *
+     * @param args - command line arguements
+     */
     public static void main(String[] args) {
         Application.launch(Connect4View.class, args);
     }
